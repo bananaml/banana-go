f293f4c49ce1374794f18e48a03b479f73502cfb
erik-dunteman <erikdunteman@gmail.com>
2022-04-13 20:16:59 -0500

    added carrot example

--- examples/carrot.go
+++ examples/carrot.go
@@ -1,15 +1,3 @@
-# Banana Go SDK
-
-### Getting Started
-
-Install via go get
-`go get github.com/bananaml/banana-go`
-
-Get your API Key
-- [Sign in / log in here](https://app.banana.dev)
-
-Run:
-```go
 package main
 
 import (
@@ -26,6 +14,7 @@ func main() {
 	type input struct {
 		ImageURL string `json:"imageURL"`
 	}
+
 	in := input{
 		ImageURL: "https://demo-images-banana.s3.us-west-1.amazonaws.com/image2.jpg",
 	}
@@ -41,27 +30,8 @@ func main() {
 	type output []struct {
 		Caption string `json:"caption"`
 	}
+
 	out := output{}
-
 	json.Unmarshal(bananaOut.ModelOutputs, &out)
-
 	fmt.Println(out[0].Caption)
 }
-
-```
-
-bananaOut type:
-```javascript
-{
-    "id": "12345678-1234-1234-1234-123456789012", 
-    "message": "success", 
-    "created": 1649712752, 
-    "apiVersion": "26 Nov 2021", 
-    "modelOutputs": [
-        {
-            // a json specific to your model. In this example, the caption of the image
-            "caption": "a baseball player throwing a ball", 
-        }
-    ]
-}
-```

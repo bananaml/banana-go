88fa589c7b5391f9764657888f234e15dff57e9b
erik-dunteman <erikdunteman@gmail.com>
2022-05-13 16:59:37 -0500

    working end to end with .run .start .check without API break

--- types.go
+++ types.go
@@ -4,29 +4,29 @@ import "encoding/json"
 
 // This package defines the entirety of what data can be accepted on the inbound Post requests
 
-// InStartV2 Defines the acceptable payload into the task/start/v2 endpoint
-type inStartV2 struct {
+// InStartV3 Defines the acceptable payload into the task/start/v3 endpoint
+type inStartV3 struct {
 	ID          string          `json:"id" xml:"id" form:"id"`
 	Created     int64           `json:"created" xml:"created" form:"created"`
 	APIKey      string          `json:"apiKey" xml:"apiKey" form:"apiKey"`
 	ModelKey    string          `json:"modelKey" xml:"modelKey" form:"modelKey"`
+	StartOnly   bool            `json:"startOnly" xml:"startOnly" form:"startOnly"`
 	ModelInputs json.RawMessage `json:"modelInputs" xml:"modelInputs" form:"modelInputs"` // Keeps the dynamic substruct as raw bytes
-	Config      struct {
-		MachineID string `json:"machineID" xml:"machineID" form:"machineID"`
-	} `json:"config" xml:"config" form:"config"`
 }
 
-// OutStartV1 defines the payload to be returned by the task/start/v2 endpoint
-type outStartV2 struct {
-	ID         string `json:"id" xml:"id" form:"id"`
-	Message    string `json:"message" xml:"message" form:"message"`
-	Created    int64  `json:"created" xml:"created" form:"created"`
-	APIVersion string `json:"apiVersion" xml:"apiVersion" form:"apiVersion"`
-	CallID     string `json:"callID" xml:"callID" form:"callID"`
+// OutStartV3 defines the payload to be returned by the task/start/v3 endpoint
+type outStartV3 struct {
+	ID           string          `json:"id" xml:"id" form:"id"`
+	Message      string          `json:"message" xml:"message" form:"message"`
+	Created      int64           `json:"created" xml:"created" form:"created"`
+	APIVersion   string          `json:"apiVersion" xml:"apiVersion" form:"apiVersion"`
+	CallID       string          `json:"callID" xml:"callID" form:"callID"`
+	Finished     bool            `json:"finished" xml:"finished" form:"finished"`
+	ModelOutputs json.RawMessage `json:"modelOutputs" xml:"modelOutputs" form:"modelOutputs"`
 }
 
-// InCheckV1 Defines the acceptable payload into the task/check/v2 endpoint
-type inCheckV2 struct {
+// InCheckV3 Defines the acceptable payload into the task/check/v3 endpoint
+type inCheckV3 struct {
 	ID       string `json:"id" xml:"id" form:"id"`
 	Created  int64  `json:"created" xml:"created" form:"created"`
 	APIKey   string `json:"apiKey" xml:"apiKey" form:"apiKey"`
@@ -34,11 +34,14 @@ type inCheckV2 struct {
 	CallID   string `json:"callID" xml:"callID" form:"callID"`
 }
 
-// OutCheckV1 defines the payload to be returned by the task/check/v2 endpoint
-type outCheckV2 struct {
+// OutCheckV3 defines the payload to be returned by the task/check/v3 endpoint
+type outCheckV3 struct {
 	ID           string          `json:"id" xml:"id" form:"id"`
 	Message      string          `json:"message" xml:"message" form:"message"`
 	Created      int64           `json:"created" xml:"created" form:"created"`
 	APIVersion   string          `json:"apiVersion" xml:"apiVersion" form:"apiVersion"`
 	ModelOutputs json.RawMessage `json:"modelOutputs" xml:"modelOutputs" form:"modelOutputs"`
 }
+
+// Export Result, aliased from outCheckV3, which is the return of banana.Run and banana.Check
+type Result outCheckV3
